a89f9b5ed289a995aa859ad9d6642c8867be9746
haozuoyang <haozhi152@163.com>
2021-09-19 20:34:39 +0800

    udp server&client

--- udp-client/src/main.rs
+++ udp-client/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::str;
+use std::{io, net::UdpSocket};
+fn main() -> io::Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:8000")?;
+    socket.connect("127.0.0.1:8080")?;
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        socket.send(input.as_bytes())?;
+        let mut buf = [0u8; 1024];
+        socket.recv_from(&mut buf)?;
+        println!(
+            "recieve from server:{}",
+            str::from_utf8(&buf).expect("could not write byte with string")
+        );
+    }
 }
--- udp-server/src/main.rs
+++ udp-server/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io, net::UdpSocket};
+fn main() -> io::Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:8080").expect("bind port 8080 failed");
+    loop {
+        let mut buf = [0u8; 1024];
+        let (amt, src) = socket.recv_from(&mut buf)?;
+        let buf = &mut buf[..amt];
+        buf.reverse();
+        socket.send_to(buf, &src)?;
+    }
 }
