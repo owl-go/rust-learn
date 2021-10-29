f40e0930353b852a38a0b85179f853f7adc8fb00
haozuoyang <haozhi152@163.com>
2021-10-29 15:42:54 +0800

    unsafe

--- unsafe-demo/src/main.rs
+++ unsafe-demo/src/main.rs
@@ -1,3 +1,4 @@
+static mut COUNTER: u32 = 0;
 fn main() {
     let mut num = 5;
     let r1 = &num as *const i32;
@@ -5,5 +6,24 @@ fn main() {
     unsafe {
         println!("{}", *r1);
         println!("{}", *r2);
+        *r2 = 10;
+        println!("{}", *r1);
+        println!("{}", *r2);
+        println!("{}", num);
     }
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+    let r = &mut v[..];
+    // let mut r = v;
+    let (a, b) = r.split_at_mut(3);
+    println!("{:?}-{:?}", a, b);
+
+    unsafe {
+        //修改静态变量是不安全的操作
+        COUNTER += 1;
+        println!("counter:{}", COUNTER);
+    }
+}
+#[no_mangle]
+pub extern "C" fn call_from_c() {
+    println!("Just call function from c");
 }
